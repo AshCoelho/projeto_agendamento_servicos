bb39d203378070e4f0971d0beb367120e34d1d1c
mrkevyn <kevyn.carlos@hotmail.com>
2026-02-24 21:26:04 -0300

    prod

--- src/services/atendente.api.js
+++ src/services/atendente.api.js
@@ -48,5 +48,11 @@ export const AtendenteApi = {
 
   async carregarServicos(secretariaId) {
     return await api.get(`/secretarias/${secretariaId}/servicos`)
+  },
+
+  async deslogarGuiche(gerenciadorId) {
+    // Adicionamos um log aqui para você ver se o método é chamado
+    console.log("API: Chamando logout-guiche para ID", gerenciadorId);
+    return await api.post(`/gerenciador/${gerenciadorId}/logout-guiche`);
   }
 }
